46a159d95a96e13a90a0963b8f4726e2e740472a
Rylee-Blaubergs-PCC-Student <rylee.blaubergs@pcc.edu>
2025-10-21 13:22:26 -0700

    Update Midterm.cpp

--- Midterm.cpp
+++ Midterm.cpp
@@ -1,12 +1,12 @@
 // ------------- FILE HEADER -------------
 // Author ✅: Rylee Blaubergs
-// Assignment ✅: 
+// Assignment ✅: Midterm
 // Date ✅: 
-// Citations: zyBooks
+// Citations: zyBooks, examples
 
 
 // ------------- ZYBOOKS SCORES -------------
-// Chapter ✅: 
+// Chapter ✅: none for midterm
 // Participation ✅:
 // Challenge ✅: 
 // Labs ✅: 
@@ -34,6 +34,7 @@
 // ------------- CODE -------------
 #include <iostream>
 #include <iomanip>
+#include <string>
 using namespace std;
 
 // Main function
@@ -48,7 +49,7 @@ int main(int argc, char* argv[]) {
 
 // ------------- DESIGN -------------
 /* 
-Program Name: 
+Program Name: The Rock Collecting Championships
 
 Program Description: 
 
@@ -67,8 +68,67 @@ D. LOGIC and ALGORITHMS
 
 SAMPLE RUNS:
 
+Welcome to the Rock Collector Championships!
+
+Enter player 1 name: Gordan Freeman
+How many rocks did Gordan Freeman collect? -9
+Invalid amount. 0 will be entered.
+
+Enter player 2 name: Link
+How many rocks did Link collect? 45
+
+Enter player 3 name: D. Va
+How many rocks did D. Va collect? 45
+
+Link and D. Va are tied for first place.
+Gordan Freeman is in second place!
+
+The average number of rocks collected by the top three players is 30.00 rocks!
+
+Congratulations Rock Collectors!
 
 
+-------------------------------------------------------------------------------------
+
+
+Welcome to the Rock Collector Championships!
+
+Enter player 1 name: Mario
+How many rocks did Mario collect? 56
+
+Enter player 2 name: Master Chief
+How many rocks did Master Chief collect? 56
+
+Enter player 3 name: Sonic
+How many rocks did Sonic collect? 56
+
+It is a three way tie!
+
+The average number of rocks collected by the top three players is 56.00 rocks!
+
+Congratulations Rock Collectors!
+
+
+-------------------------------------------------------------------------------------
+
+
+Welcome to the Rock Collector Championships!
+
+Enter player 1 name: King Dedede
+How many rocks did King Dedede collect? 57
+Enter player 2 name: Samus
+How many rocks did Samus collect? 102
+
+Enter player 3 name: Kirby
+How many rocks did Kirby collect? 62
+
+Samus is in first place!
+Kirby is in second place.
+King Dedede is in third place.
+
+The average number of rocks collected by the top three players is 73.67 rocks!
+
+Congratulations Rock Collectors!
 
 
 */
